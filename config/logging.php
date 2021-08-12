f7ed355c2c84e28f7cf5e35300a029d83bf925f2
ryan <ryan_li@linkWish.com>
2021-08-12 14:54:19 +0800

    feat: logging data name with user name

--- config/logging.php
+++ config/logging.php
@@ -43,13 +43,13 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/laravel-' . get_current_user() . '.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path('logs/laravel-' . get_current_user() . '.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
         ],
