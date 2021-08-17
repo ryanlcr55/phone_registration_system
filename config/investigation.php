e654c26f763040f9b2ffc5195d32b4b64ad2a39d
ryan <ryan_li@linkWish.com>
2021-08-17 15:53:27 +0800

    feat: Suspected record tracking by api

--- config/investigation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'paginating' => [
-        'number_per_page' => env('PAGINATING_NUMBER_PER_PAGE', 50),
-    ],
-
-    'get_registration_range_days' => env('GET_REGISTRATION_RANG_DAYS', 7),
-];
